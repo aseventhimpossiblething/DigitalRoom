d40a62b31396b5d01e0af82abe0b696756a1b3d0
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-01 22:33:43 -0500

    Update DisplayFiles.py

--- DisplayFiles.py
+++ DisplayFiles.py
@@ -4,5 +4,7 @@ def showfiles():
     print("os.getcwd() 1 ",os.getcwd())
     os.chdir("/GMDelight/externalDiskForDR")
     print("os.getcwd() 2 ",os.getcwd())
-    return os.getcwd();
+    contents=str("File Location = ",os.getcwd()," ::: Contents==",os.listdir())
+    #return os.getcwd();
+    return contents
 #print("!!!!!!!!!!!!!!!!!!!",showfiles(),"!!!!!!!!!!!!!!!!!!!!"); 
