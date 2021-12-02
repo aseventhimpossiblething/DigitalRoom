5b097e379213f9347abb11d5df686b8462995bef
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-01 22:56:01 -0500

    Update DisplayFiles.py

--- DisplayFiles.py
+++ DisplayFiles.py
@@ -4,7 +4,8 @@ def showfiles():
     print("os.getcwd() 1 ",os.getcwd())
     os.chdir("/GMDelight/externalDiskForDR")
     print("os.getcwd() 2 ",os.getcwd())
-    contents=str(os.getcwd(),"-",str(os.listdir()))
+    print("len(os.listdir())  ",len(os.listdir()))
+    contents=os.listdir()
     #return os.getcwd();
     return contents
 #print("!!!!!!!!!!!!!!!!!!!",showfiles(),"!!!!!!!!!!!!!!!!!!!!"); 
