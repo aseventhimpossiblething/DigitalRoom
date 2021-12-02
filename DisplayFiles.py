0bd85f15cdbc16be6d6ec4f1e1330a66dcf1fa6d
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-01 22:19:12 -0500

    Update DisplayFiles.py

--- DisplayFiles.py
+++ DisplayFiles.py
@@ -1,5 +1,6 @@
 import os
 
 def showfiles():
+    print("os.getcwd() ",os.getcwd())
     return os.getcwd();
-print(showfiles()); 
+print("!!!!!!!!!!!!!!!!!!!",showfiles(),"!!!!!!!!!!!!!!!!!!!!"); 
