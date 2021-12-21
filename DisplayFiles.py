73e400cce8dbe469bc578a0639bd3d98394d5ab2
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-21 13:59:08 -0600

    Update DisplayFiles.py

--- DisplayFiles.py
+++ DisplayFiles.py
@@ -1,4 +1,23 @@
 import os
+def SaveFileFromLoadingTemplate():
+    os.chdir("/GMDelight/view9");
+    #print(os.listdir())
+    req=request.files['sheet'];
+    reqstr=str(req);
+    print(os.listdir())
+    print(reqstr)
+    #startfn=reqstr.find("FileStorage:");
+    #print(startfn)
+    endfn=reqstr.find("(");
+    #print(endfn)
+    FileName=reqstr[15:endfn-2];
+    print("FileName - ",FileName);
+    request.files['sheet'].save(FileName);
+    #Uploads.store();
+    print(" Button clicked")
+    print("Button clicked")
+    print("Button clicked") 
+    #print("request-",request.files['sheet'])
 
 def showfiles():
     print("os.getcwd() 1 ",os.getcwd())
