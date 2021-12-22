20d355db8d69784e4a5d47192b2bada101a72734
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-22 16:17:46 -0600

    Update DisplayFiles.py

--- DisplayFiles.py
+++ DisplayFiles.py
@@ -27,12 +27,6 @@ def showfiles():
     print("os.getcwd() 2 ",os.getcwd())
     print("len(os.listdir())  ",len(os.listdir()))
     contents=os.listdir()
-    #DF=pandas.DataFrame(contents);
-    #DF.columns=["Files"];
-    
-    
-    #print("DF ",DF)
-    #cssgrid="grid-template-columns: 90px 50 px 120px"
     len(contents);
     print("len(contents) = ",len(contents));
     SecondCol=[];
@@ -40,14 +34,28 @@ def showfiles():
     while nz<len(contents):
           li=contents[nz];
           HTMLSeq='<div><form action="/delfile"><input type="hidden" name="'+li+'" value="'+li+'"><a href="/test/'+li+'">'+li+'</a> ___ <input type="submit" value="delete"></form></div>' 
-          #HTMLSeq="<div></div>"
           SecondCol.append(HTMLSeq);  
           print(li)  
           nz=nz+1;
-    #return os.getcwd();
-    #DF["Fun"]=SecondCol;
-    #print("DF ",DF)
-    #return contents
-    #return DF.to_html();
     return ''.join(SecondCol);
-#print("!!!!!!!!!!!!!!!!!!!",showfiles(),"!!!!!!!!!!!!!!!!!!!!"); 
+
+
+
+
+def showfilesV0():
+    print("os.getcwd() 1 ",os.getcwd())
+    os.chdir("/GMDelight/view9")
+    print("os.getcwd() 2 ",os.getcwd())
+    print("len(os.listdir())  ",len(os.listdir()))
+    contents=os.listdir()
+    len(contents);
+    print("len(contents) = ",len(contents));
+    SecondCol=[];
+    nz=0;
+    while nz<len(contents):
+          li=contents[nz];
+          HTMLSeq='<div><form action="/delfile"><input type="hidden" name="'+li+'" value="'+li+'"><a href="/test/'+li+'">'+li+'</a> ___ <input type="submit" value="delete"></form></div>' 
+          SecondCol.append(HTMLSeq);  
+          print(li)  
+          nz=nz+1;
+    return ''.join(SecondCol);
