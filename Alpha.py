39a785a886904c35c1ab681df1ce9c2f4de5fbe1
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-22 13:32:07 -0600

    Update Alpha.py

--- Alpha.py
+++ Alpha.py
@@ -79,7 +79,7 @@ def dfile():
     print("request ",request);
     reqstr=str(request);
     findsign=reqstr.find("=")
-    findendspace=reqstr.find(" ")
+    findendspace=reqstr.find("[")
     print("find = ",findsign)
     print("findendspace ",findendspace)
     filename=reqstr[findsign:findendspace]
