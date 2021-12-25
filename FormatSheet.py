40fd5e8dfbadb67d60760624cf13b04f9a0a7b0e
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-25 11:34:54 -0600

    Update FormatSheet.py

--- FormatSheet.py
+++ FormatSheet.py
@@ -3,6 +3,7 @@ import os
 def headers():
   print("---calling headers-----")
   print("os getcwd ",os.getcwd())
+  print("os chdir ",os.chdir('/GMDelight/DigitalRoom/CTRData'))
   print(" entered sequence ")
   print("os listdir ",os.listdir())
   return os.listdir();
