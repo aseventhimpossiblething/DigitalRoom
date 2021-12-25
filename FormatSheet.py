2b2436f745aa77408348dc96b8c1595b6927c75b
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-25 11:29:32 -0600

    Update FormatSheet.py

--- FormatSheet.py
+++ FormatSheet.py
@@ -1,4 +1,8 @@
 import os
-print("os getcwd ",os.getcwd())
-print(" entered sequence ")
-print("os listdir ",os.listdir())
+
+def headers():
+  print("---calling headers-----")
+  print("os getcwd ",os.getcwd())
+  print(" entered sequence ")
+  print("os listdir ",os.listdir())
+  return os.listdir();
