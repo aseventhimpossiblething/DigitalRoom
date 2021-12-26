2dea378f5ec72b08aaf5170456d28cc74959633b
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-26 12:14:34 -0600

    Update FormatSheet.py

--- FormatSheet.py
+++ FormatSheet.py
@@ -2,13 +2,23 @@ import os
 
 def headers():
   os.chdir('/GMDelight/DigitalRoom/Sheets/CTRData');
-  numOfSheets=len(os.listdir());
+  ActiveSheets=os.listdir();
+  numOfSheets=len(ActiveSheets);
+  if numOfSheets =0:
+     return "Enter a single csv or xlsx sheet. - DO NOT ENTER A MULTISHEET WORKBOOK! "
   print("-----------------------------------------------------------")
   print("---calling headers-----")
   print("numOfSheets ",numOfSheets)
-  #print("os chdir ",os.chdir('/GMDelight/DigitalRoom/Sheets/CTRData'))
+  #ActiveSheets=os.listdir();
+  lCount=0;
+  while lCount < numOfSheets():
+        ActiveSheet=ActiveSheets[lCount];
+        print("ActiveSheets ",ActiveSheets);
+        OpenActiveSheets=open(ActiveSheets,'r');
+        print(lCount," done")
+        
   print(" entered sequence ")
-  #print("os listdir[0] ",os.listdir()[0])
+  print("os listdir[0] ",os.listdir()[0])
   #print("os listdir[1] ",os.listdir()[1])
   #print("os listdir[2] ",os.listdir()[2])
   
