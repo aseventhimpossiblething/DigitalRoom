b491bb9e909a3160dc45a98e0206448ac8cd4704
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-26 13:23:02 -0600

    Update FormatSheet.py

--- FormatSheet.py
+++ FormatSheet.py
@@ -8,29 +8,19 @@ def headers():
      return "Enter a single csv or xlsx sheet. - DO NOT ENTER A MULTISHEET WORKBOOK! "
   print("-----------------------------------------------------------")
   print("---calling headers-----")
-  print("numOfSheets ",numOfSheets)
+  #print("numOfSheets ",numOfSheets)
   #ActiveSheets=os.listdir();
   lCount=0;
   while lCount < numOfSheets:
         ActiveSheet=ActiveSheets[lCount];
         print("ActiveSheet ",ActiveSheet);
-        tFL=os.getcwd()+ActiveSheet;
-        ftype=type(ActiveSheet);
-        print("ftype ActiveSheet ",ftype);
-        print("ftype os.getcwd() ",type(os.getcwd()));
-        print("ftype os.getcwd() ",type(os.getcwd()));
-        print("tFL ", tFL);
         try:
          OpenActiveSheets=open(ActiveSheet,'r');
+         readActiveSheets=read(ActiveSheets);
          print(" opened ",ActiveSheet)
         except:
           print(" failed to open ",ActiveSheet)
-        print(lCount," done")
+        #print(lCount," done")
         lCount=lCount+1;
-        
-  print(" entered sequence ")
-  print("os listdir[0] ",os.listdir()[0])
-  #print("os listdir[1] ",os.listdir()[1])
-  #print("os listdir[2] ",os.listdir()[2])
-  
+          
   return ActiveSheets;
