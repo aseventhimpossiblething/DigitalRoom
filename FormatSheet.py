fe5becc6e24495569c7b88c2dee4eba2d2e9554f
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-26 11:56:34 -0600

    Update FormatSheet.py

--- FormatSheet.py
+++ FormatSheet.py
@@ -8,8 +8,8 @@ def headers():
   print("numOfSheets ",numOfSheets)
   #print("os chdir ",os.chdir('/GMDelight/DigitalRoom/Sheets/CTRData'))
   print(" entered sequence ")
-  print("os listdir[0] ",os.listdir()[0])
-  print("os listdir[1] ",os.listdir()[1])
+  #print("os listdir[0] ",os.listdir()[0])
+  #print("os listdir[1] ",os.listdir()[1])
   #print("os listdir[2] ",os.listdir()[2])
   
   return os.listdir();
