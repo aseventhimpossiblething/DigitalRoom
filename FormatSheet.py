574bd049e84e699a3e5efcc8a0ed88ae5e7aca7f
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-26 19:05:53 -0600

    Update FormatSheet.py

--- FormatSheet.py
+++ FormatSheet.py
@@ -26,7 +26,7 @@ def headers():
         if isxlsx > 1:
               readActiveSheets=pandas.read_excel(ActiveSheet); 
         if iscsv > 1:
-              readActiveSheets=open(ActiveSheet,'r').read();    
+              readActiveSheets=pandas.DataFrame(open(ActiveSheet,'r').read());    
         print(" readActiveSheets ",readActiveSheets);
         lCount=lCount+1;
   return ActiveSheets;
