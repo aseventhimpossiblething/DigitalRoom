0b47f55e740d239935b78a567495ef9331821677
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-27 08:18:59 -0600

    Update FormatSheet.py

--- FormatSheet.py
+++ FormatSheet.py
@@ -15,22 +15,23 @@ def headers():
   #ActiveSheets=os.listdir();
   lCount=0;
   while lCount < numOfSheets:
-        ActiveSheet=ActiveSheets[lCount];
+        ListOfFrames=[];
         isxlsx=ActiveSheet.lower().find(".xlsx");
         iscsv=ActiveSheet.lower().find(".csv");
-        print("ActiveSheet ",ActiveSheet);
+        #print("ActiveSheet ",ActiveSheet);
         #print("isxlsx ",isxlsx); 
         #print("iscsv ",iscsv);
-        print(" opened ",ActiveSheet);
-        OpenActiveSheet=open(ActiveSheet,'r');
-        print("OpenActiveSheet ",OpenActiveSheet)
+        #print(" opened ",ActiveSheet);
+        #OpenActiveSheet=open(ActiveSheet,'r');
+        #print("OpenActiveSheet ",OpenActiveSheet)
         if isxlsx > 1:
               readActiveSheet=pandas.read_excel(ActiveSheet); 
         if iscsv > 1:
-              #readActiveSheets=open(ActiveSheet,'r').read();
               readActiveSheet=pandas.read_csv(ActiveSheet); 
               readActiveSheet=pandas.DataFrame(data=readActiveSheet); 
         def activeSheetParse(readActiveSheet):
+            global
+            ListOfFrames
             print("readActiveSheet");
             print(readActiveSheet);
             print("readActiveSheet.columns = ",readActiveSheet.columns);
@@ -45,6 +46,7 @@ def headers():
             if strconv.replace(",","")=="":
                print("emptySet!"); 
                return "Empty_File";
+            ListOfFrames.append(readActiveSheet);
             print("strconv ch 1 = ",strconv);
             strconv=strconv.split(",");
             print("strconv ch 2 = ",strconv);
@@ -52,6 +54,7 @@ def headers():
             print("strconv[0] = ",strconv[0]);
             print("strconv[1] = ",strconv[1]);
             l2count=0;
+            """
             while l2count<len(strconv):
               hcolnam=strconv[l2count];
               if hcolnam.find("\n")>-1:
@@ -62,6 +65,7 @@ def headers():
               col=readActiveSheet[hcolnam];
               print(col);
               l2count=l2count+1;
+            """
             print("ActiveSheetParse Finished")  
         activeSheetParse(readActiveSheet);
         lCount=lCount+1;
