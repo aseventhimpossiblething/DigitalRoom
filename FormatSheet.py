eb1f0e7908737f9bf3ea2b048523697aadd5a57b
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-27 13:51:04 -0600

    Update FormatSheet.py

--- FormatSheet.py
+++ FormatSheet.py
@@ -46,11 +46,13 @@ def headers():
                   dcolumn=readActiveSheet[col];
                   coltype=str(dcolumn.dtypes);
                   Qobject=coltype.find('object');
+                  """
                   print(" dcolumn ", dcolumn);
                   print("coltype ",coltype);
                   print("Qobject ",Qobject);
                   NewQcats=[];
                   OldQcats=[];  
+                  """
                   if Qobject>-1:
                      print(col," is catagorical cat process run");
                      catcount=0;
@@ -68,21 +70,25 @@ def headers():
                      ncolnam=str(col)+"_as_Cat_Var";      
                      #readActiveSheet.drop([col]);
                      readActiveSheet[ncolnam]=NewQcats; 
+                  """
                   print("col ",col);
                   print("dcolumn ", dcolumn);
                   print("coltype ", coltype);
+                  """
                   l2count=l2count+1;
             return readActiveSheet;
         aSP=activeSheetParse(readActiveSheet);
         EmptyQ=str(aSP).find("Empty_File");
         if EmptyQ<0:
            ListOfFrames.append(aSP);
+        """    
         print("EmptyQ ",EmptyQ)
         print(" str aSP ",str(aSP))
         print("type str aSP ",type(str(aSP)))
         #if aSP!="Empty_File":
-        
+        """
         lCount=lCount+1;
+  """
   print("00000000000000000000000000000000000000000000000011111111111111") 
   print("00000000000000000000000000000000000000000000000011111111111111") 
   print(ListOfFrames);  
@@ -90,6 +96,7 @@ def headers():
   print("00000000000000000000000000000000000000000000000022222222222222") 
   print("00000000000000000000000000000000000000000000000022222222222222") 
   #return ActiveSheets;
+  """
   return ListOfFrames;
 
 
