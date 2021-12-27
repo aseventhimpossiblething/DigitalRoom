486652f02d1e1ec403159d078caa1b675e58325f
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-27 00:15:54 -0600

    Update FormatSheet.py

--- FormatSheet.py
+++ FormatSheet.py
@@ -54,6 +54,7 @@ def headers():
             l2count=0;
             while l2count<len(strconv):
               hcolnam=strconv[l2count];
+              hcolnam.replace(" ","\m\")
               col=readActiveSheet[hcolnam];
               print(col);
               l2count=l2count+1;
