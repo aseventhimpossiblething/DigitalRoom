55458317eb30ffb83f77d241afc952328e310be8
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-27 00:08:41 -0600

    Update FormatSheet.py

--- FormatSheet.py
+++ FormatSheet.py
@@ -30,37 +30,33 @@ def headers():
               #readActiveSheets=open(ActiveSheet,'r').read();
               readActiveSheet=pandas.read_csv(ActiveSheet); 
               readActiveSheet=pandas.DataFrame(data=readActiveSheet); 
-        print("readActiveSheet");
-        print(readActiveSheet);
-        if str(readActiveSheet)=='Empty DataFrame':
-          print("Frame is empty");
-        print("readActiveSheet.columns = ",readActiveSheet.columns);
-        print("typeof readActiveSheet.columns = ",type(readActiveSheet.columns));
-        strconv=str(readActiveSheet.columns)
-        strconv=strconv.replace("Index(","");
-        strconv=strconv.replace("dtype='object')","");
-        #strconv=strconv.replace("],","");
-        strconv=strconv.replace("'","");
-        strconv=strconv.replace("]","");
-        strconv=strconv.replace("[","");
-        strconv=strconv.replace(", ",",");
-        #strconv=strconv.replace("'],","'");
-        #strconv=strconv.replace("[","");
-        if strconv.replace(",","")=="":
-           print("emptySet!"); 
-        print("strconv ch 1 = ",strconv);
-        strconv=strconv.split(",");
-        print("strconv ch 2 = ",strconv);
-        print("typeof strconv = ",type(strconv));
-        print("strconv[0] = ",strconv[0]);
-        print("strconv[1] = ",strconv[1]);
-        l2count=0;
-        while l2count<len(strconv):
-              #if strconv[l2count]=='':
-                 
+        def activeSheetParse(readActiveSheet):
+            print("readActiveSheet");
+            print(readActiveSheet);
+            print("readActiveSheet.columns = ",readActiveSheet.columns);
+            print("typeof readActiveSheet.columns = ",type(readActiveSheet.columns));
+            strconv=str(readActiveSheet.columns)
+            strconv=strconv.replace("Index(","");
+            strconv=strconv.replace("dtype='object')","");
+            strconv=strconv.replace("'","");
+            strconv=strconv.replace("]","");
+            strconv=strconv.replace("[","");
+            strconv=strconv.replace(", ",",");
+            if strconv.replace(",","")=="":
+               print("emptySet!"); 
+               return "Empty_File";
+            print("strconv ch 1 = ",strconv);
+            strconv=strconv.split(",");
+            print("strconv ch 2 = ",strconv);
+            print("typeof strconv = ",type(strconv));
+            print("strconv[0] = ",strconv[0]);
+            print("strconv[1] = ",strconv[1]);
+            l2count=0;
+            while l2count<len(strconv):
               hcolnam=strconv[l2count];
               col=readActiveSheet[hcolnam];
               print(col);
               l2count=l2count+1;
+            print("ActiveSheetParse Finished")  
         lCount=lCount+1;
   return ActiveSheets;
