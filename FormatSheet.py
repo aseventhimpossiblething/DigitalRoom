2bb03851ea80f28af220e702d58dca4749d17a6d
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-30 17:41:30 -0600

    Update FormatSheet.py

--- FormatSheet.py
+++ FormatSheet.py
@@ -248,7 +248,7 @@ def RegCorDescShift():
           #AboveBelowMean(UpperHalf);
           print("type UpperHalf ",type(UpperHalf))
           print("UpperHalf ",UpperHalf)
-          print("float UpperHalf mean ",statistics.mean(UpperHalf))
+          print("float UpperHalf mean ",statistics.mean(list(UpperHalf)))
           UpperQuartilesAtMean=AboveBelowMean(UpperHalf);
           #UpperQuartilesAtMean[0]
           #print('UpperQuartilesAtMean ',UpperQuartilesAtMean[0]);
