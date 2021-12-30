1c6c19c29a0a96e29ea0ace096d7793dfc6ec048
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2021-12-30 15:06:45 -0600

    Update FormatSheet.py

--- FormatSheet.py
+++ FormatSheet.py
@@ -247,7 +247,8 @@ def RegCorDescShift():
           UpperHalf=splitAtMean[0]
           #AboveBelowMean(UpperHalf);
           print("type UpperHalf ",type(UpperHalf))
-          #UpperQuartilesAtMean=AboveBelowMean(UpperHalf);
+          UpperQuartilesAtMean=AboveBelowMean(UpperHalf)[0];
+          print('UpperQuartilesAtMean ',UpperQuartilesAtMean);
           #LowerQuartilesAtMean=AboveBelowMean(pandas.DataFrame(splitAtMean[1]));
           #HighstQuartile=pandas.DataFrame(UpperQuartilesAtMean[0]);
           #LowestQuartile=pandas.DataFrame(LowerQuartilesAtMean[1]);
