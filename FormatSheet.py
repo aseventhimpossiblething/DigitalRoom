7f6668e8916f495bdcdd7907f8233d61de557899
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2022-01-02 12:45:03 -0600

    Update FormatSheet.py

--- FormatSheet.py
+++ FormatSheet.py
@@ -50,7 +50,7 @@ def headers():
               os.chdir('/GMDelight/DigitalRoom/Sheets/CTRData');
               print('xlsx listdir ',os.listdir());
               print('ActiveSheet ',ActiveSheet);
-              readActiveSheet=pandas.read_excel(ActiveSheet); 
+              readActiveSheet=pandas.read_excel("/"+ActiveSheet); 
         if iscsv > 1:
               readActiveSheet=pandas.read_csv(ActiveSheet); 
               readActiveSheet=pandas.DataFrame(data=readActiveSheet);
