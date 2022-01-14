3520415005783a41de661c42e85f84e21bc3b65e
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2022-01-14 10:59:44 -0600

    Update FormatSheet.py

--- FormatSheet.py
+++ FormatSheet.py
@@ -311,10 +311,11 @@ def RegCorDescShift():
        seaborn.heatmap(relations);
        plt.savefig("heatmap.png",bbox_inches='tight' )  
        #selectedFrame.plot(kind='hist');
-       #selectedFrame.plot(kind='bar');
+       plt.figure(figsize=(5,8))
+       selectedFrame.plot(kind='bar');
        #plt.figure().set_figwidth(10)
        #plt.figure()
-       plt.bar(selectedFrame); 
+       #plt.bar(selectedFrame); 
        
     
        plt.savefig("selectedFrame.png")
