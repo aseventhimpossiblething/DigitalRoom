766fe2ed565579da59e7b76bdb2665deafa4dd12
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2022-07-18 15:44:58 -0500

    Update FormatSheet.py

--- FormatSheet.py
+++ FormatSheet.py
@@ -322,7 +322,8 @@ def RegCorDescShift():
        print(os.getcwd());
        print(os.listdir());
        print(selectedFrame); 
-       selectedFrame.plot(kind='bar');
+       selectedFrame.plot();
+       #selectedFrame.plot(kind='bar');
        print("heatmap saved 3")
         
       
