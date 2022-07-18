57c867a3fe23e5a9d5444975b6ef5481477364ac
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2022-07-18 16:00:52 -0500

    Update FormatSheet.py

--- FormatSheet.py
+++ FormatSheet.py
@@ -358,7 +358,7 @@ def RegCorDescShift():
     #os.chdir('/GMDelight/DigitalRoom/static/');
     #plt.savefig("heatmap.png",bbox_inches='tight' )
     
-    #plt.savefig("selectedFrame.png")
+    plt.savefig("selectedFrame.png")
     print("image saved")
    
     lorem="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
