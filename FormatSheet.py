7d69dbe93a2c28dcb7a40df02802330b83047383
Michael Cherisol <50784413+aseventhimpossiblething@users.noreply.github.com>
2022-07-20 11:57:15 -0500

    Update FormatSheet.py

--- FormatSheet.py
+++ FormatSheet.py
@@ -324,8 +324,11 @@ def RegCorDescShift():
        #selectedFrame.iloc[1];
        print(' print 0 ',selectedFrame.columns.values[0]); 
        print(' print 1 ',selectedFrame.columns.values[1]); 
-       print(' print 2 ',selectedFrame.columns.values[2]);  
-       selectedFrame.plot(x=selectedFrame.columns.values[1], y=selectedFrame.columns.values[2]);
+       print(' print 2 ',selectedFrame.columns.values[2]);
+       x=[5,8,4,3,7,10]
+       y=[6,5,6,10,5,8] 
+       selectedFrame.plot(x,y)
+       #selectedFrame.plot(selectedFrame.columns.values[1], selectedFrame.columns.values[2]);
        #selectedFrame.plot(x=selectedFrame.columns.values[1], y=selectedFrame.columns.values[1]); 
        #selectedFrame.plot.hist(subplots=True); 
        #selectedFrame.hist(subplots=True);
